87c0d0bb2731a8736e6924ea1c546257873b192a
Lucas Schoch <lucas.schoch@atitude.com.br>
2018-03-26 15:49:01 -0300

    First Commit

---
+++ ExemploAtualizacoes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploAtualizacoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UpdateSimples();
+            //UpdateDependente();            
+        }
+
+        private static void UpdateDependente()
+        {
+            using (ClarifyDBEntities ctx = new ClarifyDBEntities())
+            {
+                var pessoa = ctx.Pessoas.First(x => x.ID == 12);
+                pessoa.PrimeiroNome = "Ronaldão";
+
+                var aluno = ctx.Alunos.First(x => x.IdPessoa == 12);
+                aluno.Empresa = "Santander";
+
+                ctx.SaveChanges();
+            }
+        }
+
+        private static void UpdateSimples()
+        {
+            using (ClarifyDBEntities ctx = new ClarifyDBEntities())
+            {
+                var pessoa = ctx.Pessoas.First(x => x.ID == 36);
+                pessoa.PrimeiroNome = "Geraldino";
+
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
